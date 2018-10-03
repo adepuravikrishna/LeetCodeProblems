8390b6a127cca317141ad6732bb6a842c80751e8
adepuravikrishna <adepuravikrishna@gmail.com>
2018-10-02 17:57:13 -0700

    add

--- src/com/ravikrishna/adepu/arraysandstrings/IncreasingTripletSequence.java
+++ src/com/ravikrishna/adepu/arraysandstrings/IncreasingTripletSequence.java
@@ -13,21 +13,24 @@ public class IncreasingTripletSequence {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] arr = {5,1,5,5,2,5,4 };
+		int[] arr = { 5, 1, 5, 5, 2, 5, 4 };
 		boolean check = checkIfIncreasingSequence(arr);
 		System.out.println(check);
 
 	}
 
 	private static boolean checkIfIncreasingSequence(int[] arr) {
-	    int small = Integer.MAX_VALUE, secondBig= Integer.MAX_VALUE;
-	    for(int n:arr) {
-	    	if(n<=small) small =n ;
-	    	else if(n<= secondBig) secondBig = n;
-	    	else return true;
-	    }
-	    
-	    return true;
+		int small = Integer.MAX_VALUE, secondBig = Integer.MAX_VALUE;
+		for (int n : arr) {
+			if (n <= small) {
+				small = n;
+			} else if (n <= secondBig) {
+				secondBig = n;
+			} else
+				return true;
+		}
+
+		return false;
 	}
 
 }
